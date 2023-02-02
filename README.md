9d6612f1c9b61652e768be170d785e1acde153a6
adrianaVlongo <66226590+AdrianaViabL@users.noreply.github.com>
2023-02-02 17:05:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# imersao-cloud-bootcamp-2023
+ curso feito em fevereiro de 2023 com exemplo pratico de utilização de ferramentas devops para trabalhar com o ambiente cloud
---
+++ aula2-pratica-aws-terraform/mail.tf
@@ -0,0 +1,20 @@
+terraform {
+  
+  required_providers {
+    aws = { 
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+  
+}
+
+provider "aws" {
+     region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "s3_bucket" { #criando um bucket dentro do aws
+  bucket = "teste-tcb10"
+}
+
